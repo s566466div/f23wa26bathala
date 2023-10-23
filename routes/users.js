0107a0f180452c4d3af8aa1d0085b6d6d48c3dcf
Divya Bathala <S566466@nwmissouri.edu>
2023-10-23 12:19:18 -0500

    updated appjs

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  let sum = 0;
+  let more = 0;
+  more++;
+  sum += more;
+  const responseText = `Sum is: ${sum}`;
+
   res.send('respond with a resource');
 });
+
 
 module.exports = router;
